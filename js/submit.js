eb7d5465d2c25cc4fee182499c4570509ebfda3c
Mai Anh Bắc <maianhbac17052001@gmail.com>
2024-04-09 17:32:13 +0700

    update

--- js/submit.js
+++ js/submit.js
@@ -6,12 +6,18 @@ document.addEventListener("DOMContentLoaded", () => {
     const btn = document.getElementById('btn-submit')
     btn.addEventListener('click', (e) => {
         e.preventDefault();
+        let gender = ''
+        if (sex_girl.checked) {
+            gender = sex_girl.value;
+        } else {
+            gender = sex_boy.value;
+        }
         const data = {
             name: name.value,
-            sex_girl: sex_girl.checked,
-            sex_boy: sex_boy.checked,
+            gender: gender,
             wish: wish.value,
         }
+      
         postDataGG(data);
         alert("Gửi lời chúc thành công! Cảm ơn bạn nhiều nha ");
     })
@@ -19,6 +25,7 @@ document.addEventListener("DOMContentLoaded", () => {
         const formURL = "https://docs.google.com/forms/d/e/1FAIpQLSeVmL8v9DdL5PhLlIGj706CJoZ_QocU6Ogy2PPkqw5hdGyQLA/formResponse";
         const formData = new FormData()
         formData.append('entry.916123779', data.name);
+        formData.append('entry.149657587', data.gender);
         formData.append('entry.660451796', data.wish);
         await fetch(formURL, {
             method: "POST",
