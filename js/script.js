4c7c7db957ef0094542c0c74b66f1e0a0e6c1497
Mai Anh Bắc <maianhbac17052001@gmail.com>
2024-04-09 10:31:55 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const countDownDate = new Date("April 28, 2024 11:00:00").getTime();
+const countdown = setInterval(function() {
+    const now = new Date().getTime();
+    const distance = countDownDate - now;
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / ( 1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const second = Math.floor((distance % (1000 * 60)) / 1000);
+    document.getElementById("day").innerHTML= days;
+    document.getElementById("hours").innerHTML= hours;
+    document.getElementById("minutes").innerHTML= minutes;
+    document.getElementById("second").innerHTML= second;   
+})
